fa5b2106324f721b0ff45bc8406ad13a9de9fccc
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-05-22 15:09:45 -0400

    optionChanged() event handler

--- Part-1/static/app.js
+++ Part-1/static/app.js
@@ -1,56 +1,59 @@
 
+function optionChanged() {
+  d3.json("../data/samples.json").then((importedData) => {
+    console.log(importedData);
+    // Declare variables for objects in imported data
+    var data = importedData;
+    
+    var names = importedData.names;
+    var metadata = importedData.metadata;
+    var samples = importedData.samples;
 
-d3.json("../data/samples.json").then((importedData) => {
-        console.log(importedData);
-        // Declare variables for objects in imported data
-        var data = importedData;
-        
-        var names = importedData.names;
-        var metadata = importedData.metadata;
-        var samples = importedData.samples;
+    // sample ids (153)-- of each 153 need to sort top 10 otu_id within each
+    // create empty list to hold variables 
+    labels=[];
+    values=[];
+    hovers=[];
 
-        // sample ids (153)-- of each 153 need to sort top 10 otu_id within each
-        // create empty list to hold variables 
-        labels=[];
-        values=[];
-        hovers=[];
+    // id needs to be selected first (i= selected individual (samples.id))
+    for(i=0; i<10; i++){
+        var value = samples[i].sample_values;
+        values.push(value);
 
-        for(i=0; i<10; i++){
-            var value = samples[i].sample_values;
-            values.push(value);
+        var label = samples[i].otu_ids;
+        labels.push(label);
 
-            var label = samples[i].otu_ids;
-            labels.push(label);
-
-            var hover = samples[i].otu_labels;
-            hovers.push(hover);
-        };
-        console.log(values);
-        console.log(labels);
-        console.log(hovers);
+        var hover = samples[i].otu_labels;
+        hovers.push(hover);
+    };
+    console.log(values);
+    console.log(labels);
+    console.log(hovers);
 
 
-        
+    
 
-        var trace1 = {
-            x: values,
-            y: labels,
-            text: hovers,
-            name: "Samples",
-            type: "bar",
-            orientation: "h"
-          };
-        var data =[trace1];
-        var layout = {
-            title: "Sample information",
-            margin: {
-              l: 100,
-              r: 100,
-              t: 100,
-              b: 100
-            }
-          };
-          
-          // Render the plot to the div tag with id "bar"
-          Plotly.newPlot("bar", data, layout);
-    });
+    var trace1 = {
+        x: values,
+        y: labels,
+        text: hovers,
+        name: "Samples",
+        type: "bar",
+        orientation: "h"
+      };
+    var data =[trace1];
+    var layout = {
+        title: "Sample information",
+        margin: {
+          l: 100,
+          r: 100,
+          t: 100,
+          b: 100
+        }
+      };
+      
+      // Render the plot to the div tag with id "bar"
+      Plotly.newPlot("bar", data, layout);
+});
+}
+
