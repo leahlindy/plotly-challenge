f3af7a8e2cd6c92fa1264e8629fb9bd062710d3c
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-05-24 12:59:57 -0400

    final form append

--- Part-1/static/script.js
+++ Part-1/static/script.js
@@ -1,31 +1,19 @@
 // code for number of test subject in form selection
 d3.json("data/samples.json").then((importedData) => {
-    console.log(importedData);
+    
     var data = importedData;
     var length = data.names.length;
-    console.log(length);
-
+    
     var form = d3.select("#selDataset");
+    
     for (i=0;i<length;i++){
-        form
-        .append("option")
+        form.append("option")
         .attr("class","testSubject")
         .text(i+1);
     }
 });
 
-    // alternative way
-    // var select= d3.select('select').on('change',onchange);
-
-    // var options = select
-    //     .selectAll('option')
-    //     .data(length).enter()
-    //     .append('option')
-    //     .text(function (d) { return d; });
-    // function onchange(){
-    //     selectValue = d3.select('select').property('value');
-    //     console.log(selectValue);
-    //} 
+    
 
 // function onchange() {
 // 	selectValue = d3.select('select').property('value')
