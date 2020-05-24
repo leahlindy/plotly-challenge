f190689a5269dca7aa401c74506f0c915adf2232
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-05-24 14:24:06 -0400

    getPlots function

--- Part-1/static/app.js
+++ Part-1/static/app.js
@@ -1,79 +1,110 @@
-
-// function optionChanged() {
-//   var selectID = d3.select("#selDataset").node.value();
-//   console.log(selectID);}
-
-//   d3.json("data/samples.json").then((importedData) => {
-//     console.log(importedData);
-//     // Declare variables for objects in imported data
-//     var data = importedData;
-    
-//     var names = importedData.names;
-//     var metadata = importedData.metadata;
-//     var samples = importedData.samples;
-    
-//     // sample ids (153)-- of each 153 need to sort top 10 otu_id within each
-//     // create empty list to hold variables 
-//     labels=[];
-//     values=[];
-//     hovers=[];
-
-//     // id needs to be selected first (i= selected individual (samples.id))
-//     for(i=0; i<10; i++){
-//         var value = samples[i].sample_values;
-//         values.push(value);
-
-//         var label = samples[i].otu_ids;
-//         labels.push(label);
-
-//         var hover = samples[i].otu_labels;
-//         hovers.push(hover);
-//     };
-//     console.log(values);
-//     console.log(labels);
-//     console.log(hovers);
-
-
-//     var trace1 = {
-//         x: values,
-//         y: labels,
-//         text: hovers,
-//         name: "Samples",
-//         type: "bar",
-//         orientation: "h"
-//       };
-//     var data =[trace1];
-//     var layout = {
-//         title: "Sample information",
-//         margin: {
-//           l: 100,
-//           r: 100,
-//           t: 100,
-//           b: 100
-//         }
-//       };
+function getPlots(id) {
+  //Read samples.json
+      d3.json("data/samples.json").then (data =>{
+          console.log(data)
+          var samples = data.samples;
+          
+          var ids = samples[0].otu_ids;
+          console.log(ids)
+          
+          var sampleValues =  samples[0].sample_values.slice(0,10).reverse();
+          console.log(sampleValues)
+          
+          var labels =  samples[0].otu_labels.slice(0,10);
+          console.log (labels)
+          
+      // need only top 10 (slice(0,10)) otu in reversed order 
+          var OTU_top = (samples[0].otu_ids.slice(0, 10)).reverse();
       
-//       // Render the plot to the div tag with id "bar"
-//       Plotly.newPlot("bar", data, layout);
-// });
-
-
-// optionChanged();
-
-// // function to get necessary data based on id selected
-// function getData(id) {
-//   var dropdownMenu = d3.select("#selDataset"); //the id of dropdown
-//   var dataset = dropdownMenu.property("value"); // value to the menu options
-//   var data =[]; //empty array for countrys data (each option):
-//   console.log(dataset);
-
-//   // use case to define which data will be used for updatePlotly (153 ids)
+       // get otu ID based on the top 10 OTU (return OTU #)
+          var OTU_id = OTU_top.map(d => "OTU " + d);
+          console.log(`OTU IDS: ${OTU_id}`)
+       
+          // get the top 10 labels for the plot
+          var labels =  samples[0].otu_labels.slice(0,10);
+          console.log(`OTU_labels: ${labels}`)
+          var trace1 = {
+              x: sampleValues,
+              y: OTU_id,
+              text: labels,
+              marker: {
+              color: 'lightblue'},
+              type:"bar",
+              orientation: "h",
+          };
+          // create data variable
+          var data1 = [trace1];
   
-//   //updatePlotly(data); // now update the call function given the data selected
-//   }
+          // create layout variable to set plots layout
+          var layout_1 = {
+              title: "Top 10 OTU",
+              yaxis:{
+                  tickmode:"linear",
+              },
+              margin: {
+                  l: 100,
+                  r: 100,
+                  t: 100,
+                  b: 30
+              }
+          };
+  
+          // create the bar plot
+      Plotly.newPlot("bar", data1, layout_1);
+          // The bubble chart
+          var trace2 = {
+              x: samples[0].otu_ids,
+              y: samples[0].sample_values,
+              mode: "markers",
+              marker: {
+                  size: samples[0].sample_values,
+                  color: samples[0].otu_ids
+              },
+              text:  samples[0].otu_labels
+  
+          };
+  
+          // set the layout for the bubble plot
+          var layout_2 = {
+              xaxis:{title: "OTU ID"},
+              height: 600,
+              width: 1000
+          };
+  
+          // creating data variable 
+          var data2 = [trace2];
+  
+      // create the bubble plot
+      Plotly.newPlot("bubble", data2, layout_2); 
+      
+      });
+  }  
 
-// form = d3.selectAll("#selDataset"); // define the form (ID selection)
-// form.on("change", getData); // event triggers get Data event handler
+// create the function to get the necessary demographic data (based on selected id)
+function getDemoData(id) {
+  // read the json file to get data
+      d3.json("data/samples.json").then((data)=> {
+  
+    // get the metadata info (demographic panel)
+          var metadata = data.metadata;
+  
+          console.log(metadata)
+  
+        // filter meta data info by id
+         var result = metadata.filter(meta => meta.id.toString() === id)[0];
+        // select demographic panel to put data
+         var demographicInfo = d3.select("#sample-metadata");
+          
+       // empty the demographic info panel each time before getting new id info
+         demographicInfo.html("");
+  
+       // grab the necessary demographic data data for the id and append the info to the panel
+          Object.entries(result).forEach((key) => {   
+              demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+          });
+      });
+  }
+
 
 // create the event handler for when id is selected ("change event")
 function optionChanged(id) {
@@ -102,6 +133,7 @@ function init() {
       // call the functions to display the data and the plots to the page
       //updatePlotly(data.names[0]);
       getData(data.names[0]);
+      getPlots(data.names[0]);
   });
 }
 // call init function to get initial rendering when page loads
